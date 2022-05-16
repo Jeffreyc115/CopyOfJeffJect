4be0912210e47f6bd0c06514e392199616202a92
BT_1E10_09 <dariusj0714@gmail.com>
2022-05-16 11:59:57 -0400

    safety update

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc =  new Scanner(System.in);
+       /* Scanner sc =  new Scanner(System.in);
         String name= "";
-        String size;
+        int size;
         while (!(name.length()>0)){
         System.out.println("Enter your name: ");
         name = sc.nextLine();
+        System.out.println("Enter the Dimensions of your Bingo Board.");
+        size = sc.nextInt();*/
+
+        try
+        {
+            String filePath = "src/File";
+            Scanner fileRead = new Scanner(new File(filePath));
+            if(fileRead.hasNext()) {
+                String name = fileRead.nextLine();
+            }
+            else // NO NAME DEFINED
+            {
+
+            }
+            if(fileRead.hasNext())
+            {
+                int win = Integer.parseInt(fileRead.nextLine());
+            }
+            //System.out.println(name + " " + win);
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+
+
 
         }
 
 
-        System.out.println("Enter the Dimensions of your Bingo Board.");
+
 
     }
-}
+
--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,33 @@
 import java.util.Scanner;
 public class Manager {
+    String name;
+    int wins;
+    int bS;
+public Manager (String name, int wins, int bS){
+    this.name=name;
+}
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getbS() {
+        return bS;
+    }
+
+    public void setbS(int bS) {
+        this.bS = bS;
+    }
 }
